45b0559226a93b7414764ab2e36ac063518b8838
Bogdan Oshatinskiy <limassola@yandex.ru>
2023-07-20 18:43:23 +0300

    feat: Реализовано логирование

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ const cookieParser = require('cookie-parser');
 const router = require('./routes');
 const { createUser, login } = require('./controllers/users');
 // eslint-disable-next-line import/order
-const { celebrate, Joi } = require('celebrate');
+const { celebrate, Joi, errors } = require('celebrate');
 const errorHandler = require('./middlewares/error');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const app = express();
 
@@ -16,6 +17,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/bitfilmsdb', {
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(requestLogger);
 
 app.post('/signin', celebrate({
   body: Joi.object().keys({
@@ -33,6 +35,8 @@ app.post('/signup', celebrate({
 }), createUser);
 
 app.use(router);
+app.use(errorLogger);
+app.use(errors());
 app.use(errorHandler);
 app.listen(3000, () => {
   console.log('все работает');
